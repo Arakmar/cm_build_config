1e472e6b6d6c0a3210f82abb94dd7063c282c1ee
André Pinela <sheffzor@gmail.com>
2013-11-27 23:13:51 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,1 @@
-export PATH="$PATH:/opt/local/bin/:/usr/local/bin/"
-
-export CLEAN=false
-export LUNCH=cm_galaxysl-userdebug
-export REPO_BRANCH=cm-11.0
-export RELEASE_TYPE=CM_NIGHTLY
+echo "BootStrap - Nothing to do"
